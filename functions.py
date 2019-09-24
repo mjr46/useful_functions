d76876b763f76bbf98c378ec780b078e8d786b8b
James Rand <mjr46@bath.ac.uk>
2019-09-24 15:05:49 +0000

    reverted file back by one version

--- functions.py
+++ functions.py
@@ -4,10 +4,6 @@ def mean(this_list):
 // this does bugger all  but its an example
 
 
-
-IVE dones something bad   added and commited changes and buggered up things with this programme
-
-
 total=0
 
 
