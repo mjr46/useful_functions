d511eac2149d7c4689f3f6595412aa14c7d75137
James Rand <mjr46@bath.ac.uk>
2019-09-24 14:52:58 +0000

    Merrily did some changes that actually buggered things up

--- functions.py
+++ functions.py
@@ -4,6 +4,10 @@ def mean(this_list):
 // this does bugger all  but its an example
 
 
+
+IVE dones something bad   added and commited changes and buggered up things with this programme
+
+
 total=0
 
 
